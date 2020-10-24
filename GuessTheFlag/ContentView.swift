03a401c84bb22e2f30e79e42396aac40ce520e52
Pavel Sakhanko <iospaveldev@gmail.com>
2020-10-24 17:52:07 +0300

    feat: added basic UI & flag assets

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,7 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private let countries =  ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    private let correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 25) {
+                VStack {
+                    Text("Tap the flag...")
+                        .foregroundColor(.white)
+                    Text("\(countries[correctAnswer])")
+                        .foregroundColor(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        //
+                    }, label: {
+                       Image("\(countries[number])")
+                        .renderingMode(.original)
+                    })
+                }
+                
+            }
+
+            Spacer()
+        }
+        
         Text("Hello, world!")
             .padding()
     }
