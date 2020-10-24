b6f04c302f35052ee2f5a75ec66c1e5421148902
Pavel Sakhanko <iospaveldev@gmail.com>
2020-10-24 22:28:02 +0300

    feat: Challenge

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -6,6 +6,10 @@
 //
 
 import SwiftUI
+enum ScoreTitle: String {
+    case correct = "Correct"
+    case wrong = "Wrong"
+}
 
 struct ContentView: View {
     
@@ -13,6 +17,8 @@ struct ContentView: View {
     @State private var scoreTitle = ""
     @State private var countries =  ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var incorrectAnswer = ""
+    @State private var userScore = "0"
     
     var body: some View {
         ZStack {
@@ -20,6 +26,11 @@ struct ContentView: View {
             .edgesIgnoringSafeArea(.all)
             
             VStack(spacing: 25) {
+
+                Label("User Score is \(userScore)", systemImage: "")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                
                 VStack {
                     Text("Tap the flag...")
                         .foregroundColor(.white)
@@ -46,26 +57,47 @@ struct ContentView: View {
             Spacer()
         }
         .alert(isPresented: $showingScore, content: {
-            Alert(title: Text("\(scoreTitle)"),
-                  message: Text("Your score is ???"),
-                  dismissButton: .default(Text("Continue")) {
-                      askQuestion()
-                  }
-        )}
+            Alert(title: Text(scoreTitle == ScoreTitle.wrong.rawValue ?
+                                "Wrong! That’s the flag of \(incorrectAnswer)" : "\(scoreTitle)"),
+              message: Text("Your score is \(userScore)"),
+              dismissButton: .default(Text("Continue")) {
+                  askQuestion()
+              })
+        }
     )}
 
     func flagTapped(_ number: Int) {
         showingScore = true
+
         guard number == correctAnswer else {
-            scoreTitle = "Wrong"
+            incorrectAnswer = countries[number]
+            scoreTitle = ScoreTitle.wrong.rawValue
+            countScore(userScore: userScore, scoreTitle: scoreTitle)
             return
         }
-        scoreTitle = "Correct"
+        scoreTitle = ScoreTitle.correct.rawValue
+        countScore(userScore: userScore, scoreTitle: scoreTitle)
     }
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func countScore(userScore: String, scoreTitle: String) {
+        guard var userScore = Int(userScore) else { return }
+        
+        switch scoreTitle {
+        case ScoreTitle.correct.rawValue:
+            userScore += 1
+        case ScoreTitle.wrong.rawValue:
+            while userScore > 0 {
+                userScore -= 1
+            }
+        default:
+            break
+        }
+        self.userScore = "\(userScore)"
     }
 }
 
